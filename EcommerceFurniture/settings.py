71e974acdce81d457bbfbf855e411826f72e5558
DME03 <dme.globaliasoft@gmail.com>
2022-08-22 14:08:33 +0530

    MySQL Database

--- EcommerceFurniture/settings.py
+++ EcommerceFurniture/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'EcommerceFurniture.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.mysql', 
+    'NAME': 'furniture',
+    'USER': 'root',
+    'PASSWORD': 'password123#',
+    'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+    'PORT': '3306',
+    
+} }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
